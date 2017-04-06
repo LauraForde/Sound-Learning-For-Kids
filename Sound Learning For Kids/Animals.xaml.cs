0ef49b1be9a0eef53c6d10539f6b047651a12167
LauraForde <G00315765@gmit.ie>
2017-04-06 15:40:15 +0100

    All animals added and making sounds

--- Sound Learning For Kids/Animals.xaml.cs
+++ Sound Learning For Kids/Animals.xaml.cs
@@ -35,5 +35,73 @@ namespace Sound_Learning_For_Kids
         {
             lion.Play();
         }
+        public void cat_click(object sender, RoutedEventArgs e)
+        {
+            cat.Play();
+        }
+        public void dog_click(object sender, RoutedEventArgs e)
+        {
+            dog.Play();
+        }
+        public void pig_click(object sender, RoutedEventArgs e)
+        {
+            pig.Play();
+        }
+        public void bear_click(object sender, RoutedEventArgs e)
+        {
+            bear.Play();
+        }
+        public void bee_click(object sender, RoutedEventArgs e)
+        {
+            bee.Play();
+        }
+        public void chicken_click(object sender, RoutedEventArgs e)
+        {
+            chicken.Play();
+        }
+        public void cow_click(object sender, RoutedEventArgs e)
+        {
+            cow.Play();
+        }
+        public void duck_click(object sender, RoutedEventArgs e)
+        {
+            duck.Play();
+        }
+        public void elephant_click(object sender, RoutedEventArgs e)
+        {
+            elephant.Play();
+        }
+        public void frog_click(object sender, RoutedEventArgs e)
+        {
+            frog.Play();
+        }
+        public void geese_click(object sender, RoutedEventArgs e)
+        {
+            geese.Play();
+        }
+        public void gorilla_click(object sender, RoutedEventArgs e)
+        {
+            gorilla.Play();
+        }
+        public void horse_click(object sender, RoutedEventArgs e)
+        {
+            horse.Play();
+        }
+        public void kook_click(object sender, RoutedEventArgs e)
+        {
+            kook.Play();
+        }
+        public void owl_click(object sender, RoutedEventArgs e)
+        {
+            owl.Play();
+        }
+        public void sheep_click(object sender, RoutedEventArgs e)
+        {
+            sheep.Play();
+        }
+        public void turkey_click(object sender, RoutedEventArgs e)
+        {
+            turkey.Play();
+        }
     }
 }
