5a4bcbf00cf07848fcba2bd1d399dc38caae6006
LauraForde <G00315765@gmit.ie>
2017-04-07 02:12:31 +0100

    Attempting local storage

--- Sound Learning For Kids/MainPage.xaml.cs
+++ Sound Learning For Kids/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -30,6 +31,16 @@ namespace Sound_Learning_For_Kids
         public void btn_click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Animals), null);
+
         }
+       /* ApplicationDataContainer setting = ApplicationData.Current.LocalSettings;
+        StorageFolder folder = ApplicationData.Current.LocalFolder;
+        
+        string name;
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            name.Text = TextBox.
+        }*/
     }
 }
